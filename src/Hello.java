0e154b73c33c7dcf59076e113e3e75eaeb52bcf6
littletonman <chinesejeremy@hotmail.com>
2015-06-04 11:36:50 -0700

    checking proxy

--- src/Hello.java
+++ src/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hellow world!");
 		System.out.println("I am so excited. It is finnaly working! Yeah!");
+		System.out.println("Seems that the proxy is not set correctly.");
 	}
 
 }
